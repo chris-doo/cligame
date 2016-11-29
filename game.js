b0b4e79f5f659421d409b92da08600420f68ea64
chris <fivewingedangel@gmail.com>
2016-11-29 15:53:36 -0800

    changed board to an object

--- game.js
+++ game.js
@@ -5,13 +5,35 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const board1 = ['N', 'N', 'N'];
-const board2 = ['N', 'N', 'N'];
-const board3 = ['N', 'N', 'N'];
+let win = false;
 
-rl.question(`Current Board: \n ${board1} \n ${board2} \n ${board3} \n Make a Move: `, (move) => {
+const board = {
+  0: 'N',
+  1: 'N',
+  2: 'N',
+  3: 'N',
+  4: 'N',
+  5: 'N',
+  6: 'N',
+  7: 'N',
+  8: 'N'
+}
 
-  console.log(`Move made: ${move}`);
+const board1 = [board[0], board[1], board[2]];
+const board2 = [board[3], board[4], board[5]];
+const board3 = [board[6], board[7], board[8]];
 
-  rl.close();
-});
+
+while (!win) {
+  rl.question(`Current Board: \n ${board1} \n ${board2} \n ${board3} \n Make a Move: `, (move) => {
+    
+    console.log(`Move made: ${move}`);
+    if (move === 'a') {
+      win = true;
+    }
+  });
+
+}
+
+
+rl.close();
